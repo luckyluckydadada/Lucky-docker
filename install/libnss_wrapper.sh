6f6f7d7f7f65c18f867ce552054812d6b1ff86f3
lucky <sovoki1024@gmail.com>
2020-04-17 05:53:25 +0900

    bashrc

--- install/libnss_wrapper.sh
+++ install/libnss_wrapper.sh
@@ -12,7 +12,6 @@ echo "add 'source generate_container_user' to .bashrc"
 # have to be added to hold all env vars correctly
 echo 'source $STARTUPDIR/generate_container_user' >> $HOME/.bashrc
 
-echo 'alias ls="ls --color"' >> $HOME/.bashrc
-echo 'alias ll="ls -l"' >> $HOME/.bashrc
+cat /root/.bashrc >> $HOME/.bashrc
 echo 'alias cp="cp -r"' >> $HOME/.bashrc
 echo 'alias change="chown -R 1000 ~ & chgrp -R 1000 ~"' >> $HOME/.bashrc
